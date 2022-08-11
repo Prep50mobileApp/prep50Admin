c0506a62339741f0d6f3b4225605c413a10024f7
nwabuike <nwabuikeanthony@gmail.com>
2022-08-11 15:55:12 +0100

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,122 @@ import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/LoginView.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/RegisterView.vue"),
+  },
+  {
+    path: "/register-list",
+    name: "register-list",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/Home/RegisteredView.vue"
+      ),
+  },
+  {
+    path: "/subscribers",
+    name: "subscribers",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/Home/SubscribersView.vue"
+      ),
+  },
+  {
+    path: "/unsubscribers",
+    name: "unsubscribers",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/Home/UnsubscribersView.vue"
+      ),
+  },
+  {
+    path: "/manage-blog",
+    name: "manage-blog",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/blogs/ManageBlog.vue"),
+  },
+  {
+    path: "/create-blog",
+    name: "create-blog",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/blogs/CreateBlog.vue"),
+  },
+  {
+    path: "/manage-podcast",
+    name: "manage-podcast",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/podcast/managePodcast.vue"
+      ),
+  },
+  {
+    path: "/create-podcast",
+    name: "create-podcast",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/podcast/createPodcast.vue"
+      ),
+  },
+  {
+    path: "/manage-referral",
+    name: "manage-referral",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/referral/ReferralView.vue"
+      ),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
